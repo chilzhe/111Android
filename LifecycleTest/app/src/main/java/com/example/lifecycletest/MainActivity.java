467f32484e73b68c0ecf8bb7996686b83956039c
chilzhe <jasonchiu0317@gmail.com>
2024-04-12 14:59:44 +0800

    2024-04-12

---
+++ LifecycleTest/app/src/main/java/com/example/lifecycletest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lifecycletest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static String TAG = "LifeCycleTest";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreeate!");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected  void onStart() {
+        Log.v(TAG, "onStart!");
+        super.onStart();
+    }
+    @Override
+    protected  void onRestart() {
+        Log.v(TAG, "onRestart!");
+        super.onRestart();
+    }
+    @Override
+    protected  void onResume() {
+        Log.v(TAG, "onResume!");
+        super.onResume();
+    }
+    @Override
+    protected  void onPause() {
+        Log.v(TAG, "onPause");
+        super.onPause();
+    }
+    @Override
+    protected  void onStop() {
+        Log.v(TAG, "onStop!");
+        super.onStop();
+    }
+}
