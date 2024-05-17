6869026305ed5471ea13606c479a53c7d40e6ab5
chilzhe <jasonchiu0317@gmail.com>
2024-05-17 15:01:05 +0800

    2024-05-17

--- 0510/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ 0510/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import javax.crypto.CipherInputStream;
 
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView output = (TextView) findViewById(R.id.lblOutput);
                 output.setText("你是住在:" + cities[position]);
+                Toast.makeText(MainActivity.this, "你是住在: " + cities[position],
+                        Toast.LENGTH_LONG).show();
             }
         });
     }
